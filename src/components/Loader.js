d80791f3ece8768962cfa289250f5dfb6f2b3bde
Serhii Larchenko <serhii.larchenko@gmail.com>
2018-11-16 16:01:27 +0200

    Removed: wrapper

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div>
-      <div class="lds-roller">
-        <div></div><div></div><div></div><div></div>
-        <div></div><div></div><div></div><div></div>
-      </div>
+    <div class="lds-roller">
+      <div></div><div></div><div></div><div></div>
+      <div></div><div></div><div></div><div></div>
     </div>
   )
 }
