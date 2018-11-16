8517dec9ae48822cb630dd022423cbb5b20323b6
Serhii Larchenko <serhii.larchenko@gmail.com>
2018-11-16 04:04:18 +0200

    Corrected: loader position

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div className='blank-page'>
+    <div>
       <div class="lds-roller">
         <div></div><div></div><div></div><div></div>
         <div></div><div></div><div></div><div></div>
