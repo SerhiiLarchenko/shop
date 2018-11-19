3e886bc99dedf7a1e94bdeddc26d00fd76965ea0
Serhii Larchenko <serhii.larchenko@gmail.com>
2018-11-19 16:23:37 +0200

    Added: server error handling

--- server/server.js
+++ server/server.js
@@ -1,39 +1,52 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
+const port = process.env.PORT || 8000;
 
-const serverPort = 8000;
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-       next();
- });
-
+app.use(cors());
 app.use(express.json());
-
-app.get("/products", (req, res) => {
-    res.sendFile(__dirname + "/data/products.json");
+app.use((error, req, res, next) => {
+	console.error(error.stack);
+	res.status(error.status || 500).send({
+		error: error.message
+	})
 })
 
-app.post("/order", (req,res) => {
-    let { name, email, cart} = req.body,
-        orders = [];
-    fs.readFile(__dirname + "/data/orders.json", (err,data) => {
-        if (err) console.error(err, "24");
-        if (data) orders = JSON.parse(data.toString());
-        orders.push({name, email, cart});
-        fs.writeFile(__dirname + "/data/orders.json",
-         JSON.stringify(orders), (err) =>
-           {console.error(err)});
-    });
-    
-    res.send("we will call you asap");
+app.get('/products', (req, res, next) => {
+	const productsPath = `${__dirname}/data/products.json`;
+	res.sendFile(productsPath, error => {
+		if (error) next(error);
+		else console.log('Products are sent.');
+	});
 });
 
-app.listen(serverPort);
+app.post('/order', (req, res, next) => {
+	const orderPath = `${__dirname}/data/orders.json`;
+	const {name, email, cart} = req.body;
+	fs.readFile(orderPath, (error, data) => {
+		if (error) next(error);
+		else if (data) {
+			const orders = JSON.parse(data.toString());
+			orders.push({name, email, cart});
+		  fs.writeFile(orderPath,
+		   JSON.stringify(orders), error => {
+				 if (error) next(error);
+				 else {
+					 console.log('A new order is received.');
+					 res.send();
+				 }
+			 }
+			);
+		} else {
+				const error = new Error('Not found');
+				error.status = 404;
+				next(error);
+		}
+	});
+});
 
-
-console.log("server is running");
+app.listen(port, () => 
+	console.log(`Listening on port:${port}`)
+);
