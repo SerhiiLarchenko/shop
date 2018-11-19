85fc3891c6551b698877d5d825b31618cd56e70b
Serhii Larchenko <serhii.larchenko@gmail.com>
2018-11-19 16:21:04 +0200

    Corrected: class name

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div class="lds-roller">
+    <div className="lds-roller">
       <div></div><div></div><div></div><div></div>
       <div></div><div></div><div></div><div></div>
     </div>
